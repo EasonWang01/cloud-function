7b19765aed75146adef75d2c598e293960f57fe4
eason wang <easonwang@easonde-MacBook-Pro.local>
2020-10-16 10:23:46 +0800

    init

--- run/helloworld/index.js
+++ run/helloworld/index.js
@@ -2,11 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
+  res.send(`Hello`);
 });
 
-const port = process.env.PORT || 8080;
+const port = 8080;
 app.listen(port, () => {
   console.log(`helloworld: listening on port ${port}`);
 });

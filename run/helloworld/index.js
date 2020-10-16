fb9fb14fe232d49969a2a305cd27ce4c0ca2e0be
eason wang <easonwang@easonde-MacBook-Pro.local>
2020-10-16 10:46:12 +0800

    init

--- run/helloworld/index.js
+++ run/helloworld/index.js
@@ -1,8 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send(`Hello`);
+const sampleData1 = {
+  name: "John",
+  age: 20,
+  cars: [
+    { name: "Ford", models: ["Fiesta", "Focus", "Mustang"] },
+    { name: "BMW", models: ["320", "X3", "X5"] },
+    { name: "Fiat", models: ["500", "Panda"] },
+  ],
+};
+
+app.get("/", (req, res) => {
+  res.json(sampleData1);
 });
 
 const port = 8080;
